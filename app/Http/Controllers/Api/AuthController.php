0f1bbd79eb9ae347a83624986108c4c2a7fda10e
Jorge Almarales <jorgealmarales22@gmail.com>
2023-11-16 10:29:40 -0500

    Feat: creando api

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
+   
+    public function login(Request $request)
+    {    	
+    	$credentials = $request->only('email', 'password');
+        $validator = Validator::make($credentials, [
+        'email' => 'required|string|email',
+        'password' => 'required|string|min:6',
+        ]);
+       
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => 'The fields are not correct',
+                'errors' => $validator->errors()
+            ],422);
+        }
+
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid_credentials'], 400);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+        return response()->json(compact('token'));
+        
+    }
+   
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
